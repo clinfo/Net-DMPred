20fe651f577dbcb5a90bbd9d8277e9741e20e434
Narumi Hatano <81748965+NaruHT@users.noreply.github.com>
2023-07-07 19:15:14 +0900

    Update run.sh

--- run.sh
+++ run.sh
@@ -3,6 +3,6 @@
 source ~/.bashrc
 python -m scripts.prepare_graph
 python -m scripts.prepare_variant
-python -m lib.slgcn_graph_train
-python -m lib.slgcn_graph_node_vector
-python -m lib.slgcn_sample_train -d 32
+python -m lib.graph_train
+python -m lib.graph_node_vector
+python -m lib.sample_train -d 32
